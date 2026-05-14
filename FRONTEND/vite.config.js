475b1735e79851dfe0e3be427ea717e46e677bad
Vicky-Blessings <vickyblessings19@gmail.com>
2026-05-14 23:49:36 +0300

    fix package versions

--- FRONTEND/vite.config.js
+++ FRONTEND/vite.config.js
@@ -3,5 +3,9 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  base: './', // This helps with relative paths
+  base: './',
+  server: {
+    port: 5173,
+    open: true
+  }
 })
